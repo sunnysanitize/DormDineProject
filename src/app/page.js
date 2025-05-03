4bd52c91511a30d5e1587d9487af12f55b802b99
sunnysanitize <sunnyzhang565656@gmail.com>
2025-05-03 17:05:39 -0400

    Title

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white dark:bg-black px-6 py-12">
+      <h1 className="text-5xl sm:text-6xl font-extrabold text-center text-black dark:text-white">
+        DormDine <span className="text-gray-500">– Under Development</span>
+      </h1>
+    </main>
+  );
+}
